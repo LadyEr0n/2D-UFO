2b075c66976f5b70b4186451ddd25a4dfe6eab59
LadyEr0n <43074032+LadyEr0n@users.noreply.github.com>
2018-11-27 13:24:53 -0600

    finished coding pickups

---
+++ Assets/Pickups.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour {
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            //other.gameObject.SetActive(false);
+        }
+
+
+    }
+}
